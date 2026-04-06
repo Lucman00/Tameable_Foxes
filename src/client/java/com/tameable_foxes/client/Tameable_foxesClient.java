d9fb61ccc8efd75acc00bfb787e091ee0a857a23
Lucman00 <129414800+Lucman00@users.noreply.github.com>
2026-04-06 15:09:58 +0200

    Added Fabric template for the mod.

---
+++ src/client/java/com/tameable_foxes/client/Tameable_foxesClient.java
@@ -0,0 +1,10 @@
+package com.tameable_foxes.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Tameable_foxesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
